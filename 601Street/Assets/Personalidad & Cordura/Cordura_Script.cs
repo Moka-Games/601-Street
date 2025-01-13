689a999093902b206fa26b232ca43e77bb9384e6
HugoRufete <hugorufete@gmail.com>
2025-01-13 16:27:31 +0100

    Cordura Instance

--- 601Street/Assets/Personalidad & Cordura/Cordura_Script.cs
+++ 601Street/Assets/Personalidad & Cordura/Cordura_Script.cs
@@ -4,7 +4,21 @@ using UnityEngine;
 
 public class Cordura_Script : MonoBehaviour
 {
+
+    public static Cordura_Script Instance { get; private set; }
+
     [SerializeField] private int cordura = 0;
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     public void AumentoCordura(int aumento)
     {
