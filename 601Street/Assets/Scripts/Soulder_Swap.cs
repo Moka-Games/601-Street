25d24e290a735d203e3f44f74dd56bc561a17d07
HugoRufete <hugorufete@gmail.com>
2025-01-31 22:39:55 +0100

    Soulder Camera

--- 601Street/Assets/Scripts/Soulder_Swap.cs
+++ 601Street/Assets/Scripts/Soulder_Swap.cs
@@ -3,37 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Soulder_Swap : MonoBehaviour
+public class Shoulder_Swap : MonoBehaviour
 {
-    public CinemachineFreeLook rightSoulder_Camera;
-    public CinemachineFreeLook leftSoulder_Camera;
+    public CinemachineFreeLook rightShoulder_Camera;
+    public CinemachineFreeLook leftShoulder_Camera;
 
-    private bool rightSoulder_IsActive = false;
-    private bool leftSoulder_IsActive = false;
+    private bool rightShoulder_IsActive = false;
+    private bool leftShoulder_IsActive = false;
+
     void Start()
     {
-        rightSoulder_IsActive = true;
-
+        rightShoulder_IsActive = true;
         InitializeCamera();
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            print("Input Pressed");
-            if(rightSoulder_IsActive)
+            if (rightShoulder_IsActive)
             {
-                print("Enabling Right Soulder Camera...");
-
-                Enable_LeftSoulder_Camera();
-                rightSoulder_IsActive = false;
+                print("Enabling Left Shoulder Camera...");
+                Enable_LeftShoulder_Camera();
+                rightShoulder_IsActive = false;
             }
-            else if(leftSoulder_IsActive) 
+            else if (leftShoulder_IsActive)
             {
-                print("Enabling Left Soulder Camera...");
-                Enable_RightSoulder_Camera();
-                leftSoulder_IsActive = false;
+                print("Enabling Right Shoulder Camera...");
+                Enable_RightShoulder_Camera();
+                leftShoulder_IsActive = false;
             }
             else
             {
@@ -42,32 +40,54 @@ public class Soulder_Swap : MonoBehaviour
         }
     }
 
-    public void Enable_LeftSoulder_Camera()
+    public void Enable_LeftShoulder_Camera()
     {
-        leftSoulder_Camera.enabled = true;
-        rightSoulder_Camera.enabled = false;
-        leftSoulder_IsActive = true ;
+        SetCameraPriority(11, leftShoulder_Camera);
+        SetCameraPriority(10, rightShoulder_Camera);
+        leftShoulder_IsActive = true;
+
+        AdjustTrackedObjectOffset(leftShoulder_Camera, -0.75f);
     }
-    public void Enable_RightSoulder_Camera()
+
+    public void Enable_RightShoulder_Camera()
     {
-        rightSoulder_Camera.enabled = true;
-        leftSoulder_Camera.enabled = true;
-        rightSoulder_IsActive = true ;
+        SetCameraPriority(11, rightShoulder_Camera);
+        SetCameraPriority(10, leftShoulder_Camera);
+        rightShoulder_IsActive = true;
+
+        AdjustTrackedObjectOffset(rightShoulder_Camera, 0.75f);
     }
 
     public void InitializeCamera()
     {
-        if (rightSoulder_IsActive && rightSoulder_Camera != null)
+        if (rightShoulder_IsActive && rightShoulder_Camera != null)
         {
-            Enable_RightSoulder_Camera();
+            Enable_RightShoulder_Camera();
         }
-        else if (leftSoulder_IsActive && leftSoulder_Camera != null)
+        else if (leftShoulder_IsActive && leftShoulder_Camera != null)
         {
-            Enable_LeftSoulder_Camera();
+            Enable_LeftShoulder_Camera();
         }
         else
         {
             Debug.LogError("Ninguna cámara añadida al inspector");
+        }
+    }
+
+    public void SetCameraPriority(int priority, CinemachineFreeLook camera)
+    {
+        camera.Priority = priority;
+    }
+
+    private void AdjustTrackedObjectOffset(CinemachineFreeLook camera, float offsetX)
+    {
+        if (camera != null)
+        {
+            CinemachineComposer composer = camera.GetRig(1).GetCinemachineComponent<CinemachineComposer>();
+            if (composer != null)
+            {
+                composer.m_TrackedObjectOffset.x = offsetX;
+            }
         }
     }
 }
