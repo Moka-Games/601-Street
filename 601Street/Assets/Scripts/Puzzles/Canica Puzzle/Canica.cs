7aa3d2458bb0abbdb9bc53796225e52df5759bc8
HugoRufete <hugorufete@gmail.com>
2025-02-11 17:45:59 +0100

    On Win Event (Puzzle Canica)

--- 601Street/Assets/Scripts/Puzzles/Canica Puzzle/Canica.cs
+++ 601Street/Assets/Scripts/Puzzles/Canica Puzzle/Canica.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Canica : MonoBehaviour
 {
+    public UnityEvent OnWin;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Laberinto_Win"))
         {
-            print("Win!!"); 
+            OnWin.Invoke();
         }
     }
+
+    public void WinFunction()
+    {
+        print("Has ganado!!??");
+    }
 }
+
