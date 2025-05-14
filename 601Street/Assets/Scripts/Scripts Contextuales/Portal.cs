1de80828490da44e0198c328fd6ccf3ec2949fa9
HugoRufete <hugorufete@gmail.com>
2025-05-14 22:57:24 +0200

    Implementación Básica Escena Final

--- 601Street/Assets/Scripts/Scripts Contextuales/Portal.cs
+++ 601Street/Assets/Scripts/Scripts Contextuales/Portal.cs
@@ -3,32 +3,10 @@ using UnityEngine.SceneManagement; // Necesario para cargar escenas
 
 public class Portal : MonoBehaviour
 {
-    //public GameObject panelFinal;
-
-    private void OnTriggerEnter(Collider other)
+    public void VolverMenúPrincipal()
     {
-        if (other.CompareTag("Player"))
-        {
-            //panelFinal.SetActive(true);
-        }
-    }
+        PauseMenu pauseMenu = FindAnyObjectByType<PauseMenu>();
+        pauseMenu.BackToMainMenu();
 
-    // Función que se puede asignar a un botón de la UI para reiniciar la aplicación
-    public void ReiniciarAplicacion()
-    {
-        Debug.Log("Reiniciando aplicación...");
-
-        // Recarga la escena inicial (escena 0 en el build index)
-        SceneManager.LoadScene(0);
-
-        // Alternativa: recargar la escena actual
-        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-
-        // Si necesitas también limpiar los datos guardados:
-        PlayerPrefs.DeleteAll();
-
-        // Asegúrate de que el recolector de basura libere memoria
-        Resources.UnloadUnusedAssets();
-        System.GC.Collect();
     }
 }
