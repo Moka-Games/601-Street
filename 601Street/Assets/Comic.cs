003502122c47f9d32f60a2fbceca7d4c1020497e
HugoRufete <hugorufete@gmail.com>
2025-06-01 21:30:34 +0200

    Parque sin audio

--- 601Street/Assets/Comic.cs
+++ 601Street/Assets/Comic.cs
@@ -11,6 +11,18 @@ public class Comic : MonoBehaviour
         persistentDestructible = GetComponent<PersistentDestructible>();
     }
 
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Time.timeScale = 10f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
     public void StopMusic()
     {
         audioSource.Stop();
